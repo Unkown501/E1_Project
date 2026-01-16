94fd27fd8b327157a95a21bd77af70fba8ea1efa
Unkown501 <davidk50131@gmail.com>
2026-01-15 17:39:54 -0800

    platformer start finished

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,17 +6,26 @@ public class PlayerController : MonoBehaviour
     float movementx;
     float movementy;
     [SerializeField] float speed = 5.0f;
+    Rigidbody2D rb;
+    bool onGround;
+    int score = 0;
+     [SerializeField] int jumpforce = 300;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementDistanceX = movementx * speed * Time.deltaTime;
+        /*float movementDistanceX = movementx * speed * Time.deltaTime;
         float movementDistanceY = movementy * speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);*/
+        rb.linearVelocity = new Vector2(movementx * speed, rb.linearVelocity.y);
+        if (onGround && movementy > 0)
+        {
+            rb.AddForce(new Vector2(0, jumpforce));
+        }
     }
     void OnMove(InputValue value)
     {
@@ -29,5 +38,27 @@ public class PlayerController : MonoBehaviour
         Debug.Log(movementy);
 
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            onGround = true;
+        }
 
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            onGround = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("collectible"))
+        {
+            score++;
+            other.gameObject.SetActive(false);
+            Debug.Log("socre:" + score);
+        }
+    }
 }
